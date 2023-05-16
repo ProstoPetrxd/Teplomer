cb927b0ea9f3d3ad9906b7569a9855a53a70ad73
ProstoPetrxd <112934387+ProstoPetrxd@users.noreply.github.com>
2023-05-16 12:04:14 +0200

    Update teplomer.py

--- teplomer.py
+++ teplomer.py
@@ -4,9 +4,9 @@ from urllib.request import urlopen
 
 URL = "http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754"
 
-xml = open("file.xml", "r+")
-xml.write(urlopen(URL).read().decode('utf-8'))
-xml.close()
+with open("file.xml", "r+") as xml:
+    xml.write(urlopen(URL).read().decode('utf-8'))
+    xml.close()
 
 mytree = ET.parse('file.xml')
 myroot = mytree.getroot()
