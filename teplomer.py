217fabdb5376deea59a95f56408ef05ef2338d10
ProstoPetrxd <112934387+ProstoPetrxd@users.noreply.github.com>
2023-05-16 12:30:11 +0200

    Update teplomer.py

--- teplomer.py
+++ teplomer.py
@@ -1,4 +1,4 @@
 from urllib import request
-remote_url = 'http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754'
-local_file = 'data.txt'
-request.urlretrieve(remote_url, local_file)
+URL = 'http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754'
+SOUBOR = 'data.txt'
+request.urlretrieve(URL, SOUBOR)
