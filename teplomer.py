c2d86fb15cc43b83c571d3fb74865f7a0a86643c
ProstoPetrxd <112934387+ProstoPetrxd@users.noreply.github.com>
2023-05-16 10:30:09 +0200

    Update teplomer.py

--- teplomer.py
+++ teplomer.py
@@ -1,9 +1,10 @@
 """Program, který pomocí dat z meteostanice na Gymnáziu ve Dvoře Králové vytváří teploměr"""
+import xml.etree.ElementTree as ET
 import requests
 import urllib3
-import xml.etree.ElementTree as ET
 
-url = "http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754"
+
+URL = "http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754"
 response = requests.get(url)
 with open('feed.xml', 'wb') as file:
     file.write(response.content)
