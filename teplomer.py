cc6494b5e4d7980de8cc102667996e21791368dd
ProstoPetrxd <112934387+ProstoPetrxd@users.noreply.github.com>
2023-05-16 10:33:10 +0200

    Update teplomer.py

--- teplomer.py
+++ teplomer.py
@@ -5,6 +5,6 @@ import urllib3
 
 
 URL = "http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754"
-response = requests.get(url)
+response = requests.get(URL)
 with open('feed.xml', 'wb') as file:
     file.write(response.content)
