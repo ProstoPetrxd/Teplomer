2b239f97cd419bf9f0b6ffb43ee5b685961c838e
ProstoPetrxd <112934387+ProstoPetrxd@users.noreply.github.com>
2023-05-16 10:45:37 +0200

    Update teplomer.py

--- teplomer.py
+++ teplomer.py
@@ -1,7 +1,6 @@
 """Program, který pomocí dat z meteostanice na Gymnáziu ve Dvoře Králové vytváří teploměr"""
 import xml.etree.ElementTree as ET
 import requests
-import urllib3
 
 
 URL = "http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754"
