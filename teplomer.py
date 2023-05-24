d04b104e69ba010bd9b20398cb030e45cf9f6af0
ProstoPetrxd <112934387+ProstoPetrxd@users.noreply.github.com>
2023-05-24 12:50:06 +0200

    Update teplomer.py

--- teplomer.py
+++ teplomer.py
@@ -52,8 +52,6 @@ class PyMeteo:
             return None
  
         value = self.root.find('./input/sensor[type="%s"]/value' % param).text
-        if self.debug:
-            print('%s: %s' % (param, value))
         if value != 'ERROR':
             return value
         return None
@@ -71,62 +69,99 @@ celkova_teplota = 0.0
 maximalni_teplota = 0.0
 minimalni_teplota = 100.0
 
+# Zacyklení programu do nekonečna
 while True:
+
+    # Zjištění aktuálního času
     t = datetime.datetime.now()
+
+    # Vymazání dat z tabulky každý den o půlnoci.
     if((t.hour == 0) and (t.minute == 0)):
-        ##Vymazání dat z tabulky každý den o půlnoci.
         urllib.request.urlopen("https://script.google.com/macros/s/AKfycbx8dInME9tlD57F9EjqsVx0RMmk5p4XEpGsS6hv11ggZ6jfNNxRrGLi3uXQwC3h1DG3/exec")
     
     if __name__ == '__main__':
-        if((((t.minute%2) == 0) or (t.minute == 0)) and (t.second == 0)):
-            #Pokud uběhly dvě minuty, zapsat hodnoty do tabulky.
+
+        # Zapnutí programu, pokud uběhly dvě minuty
+        if((((t.minute%1) == 0) or (t.minute == 0)) and (t.second == 0)):
+
+            # Získáni hodnot
             sensors = ['temperature']
-            print(t.hour, ":", t.minute)
             m = PyMeteo('http://moje.meteo-pocasi.cz/environment/web/me220012/xml/xml.xml?USID=1673&_=1684220025754', debug=True)
             m.download()
             nova_teplota = m.get_value(sensors[0])
-            pocet += 1
 
-            #Přidání naměřené hodnoty do průměru
-            celkova_teplota += float(nova_teplota)
+            # Kontrolní vypsání aktuálních hodnot.
+            if(t.hour < 10):
+                print("0%s:%s - Teplota: %s°C" % (t.hour, t.minute, nova_teplota))
+            elif(t.hour < 10 and t.minute < 10):
+                print("0%s:0%s - Teplota: %s°C" % (t.hour, t.minute, nova_teplota))
+            elif(t.minute < 10):
+                print("%s:0%s - Teplota: %s°C" % (t.hour, t.minute, nova_teplota))
+            else:
+                print("%s:%s - Teplota: %s°C" % (t.hour, t.minute, nova_teplota))
 
-            #Případný zápis nové maximální hodnoty
-            if(float(nova_teplota) > float(maximalni_teplota)):
-                maximalni_teplota = float(nova_teplota)
+            # Zjištění, není-li hodnota neplatná (Bez tohoto by se program jednou za čas vypl, protože naměřil chybnou hodnotu.)
+            if (nova_teplota != None):
 
-            #Případný zápis nové minimální hodnoty
-            if(float(nova_teplota) < float(minimalni_teplota)):
-                minimalni_teplota = float(nova_teplota)
+                # Přidání naměřené hodnoty do průměru
+                celkova_teplota += float(nova_teplota)
+                pocet += 1
 
-            urllib.request.urlopen("https://script.google.com/macros/s/AKfycbwywgTKg7z5HZ20MCVtSzl-UiqNcUcTdc5_4C4Jzc3B03S89ykNE-S7Yt9UcOJT735I1g/exec?value=%s" % nova_teplota)
-            if((t.hour >= 8) and (t.hour <= 22)):
-                #Program bude odesílat upozornění pouze mezi 8:00 a 22:00
-                #Pokud je celá hodina, odelsat upozornění na telefon.
-                if((t.minute == 0) and (t.second == 0)):
-                    with open('teplota.txt', 'r') as f:
-                        stara_teplota = f.read()
-                        f.close()
-                    rozdil_teplot = float(nova_teplota) - float(stara_teplota)
-                    conn = http.client.HTTPSConnection("api.pushover.net:443")
-                    conn.request("POST", "/1/messages.json",
-                    urllib.parse.urlencode({
-                        "token": "a8fafvyjck1k2de5yz3hphcku3i499",
-                        "html": "1",
-                        "user": "u586cd4vg7qf4mtgert6buum515t8u",
-                        "title": "Teplota ve Dvoře: %s°C" % nova_teplota,
-                        "message": "Rozdíl teplot za uplynulou hodinu: <b>%s°C</b>\
-                        <br>Průměrná teplota: <b>%s°C</b>\
-                        <br>Minimální teplota: <b>%s°C</b>\
-                        <br>Maximální teplota: <b>%s°C</b>" \
-                        % (str(round(rozdil_teplot, 1)), str(round(celkova_teplota/pocet, 1)), str(round(minimalni_teplota, 1)), str(round(maximalni_teplota, 1))),
-                    }), { "Content-type": "application/x-www-form-urlencoded" })
-                    conn.getresponse()
-                    print("Zpráva byla odeslána.")
-                    celkova_teplota = 0.0
-                    maximalni_teplota = 0.0
-                    minimalni_teplota = 100.0
-                    with open('teplota.txt', 'w') as f:
-                        stara_teplota = f.write(nova_teplota)
-                        f.close()
-    #Počkat 1 sekundu, aby se odeslal jen jeden záznam.
+                # Případný zápis nové maximální hodnoty
+                if(float(nova_teplota) > float(maximalni_teplota)):
+                    maximalni_teplota = float(nova_teplota)
+
+                # Případný zápis nové minimální hodnoty
+                if(float(nova_teplota) < float(minimalni_teplota)):
+                    minimalni_teplota = float(nova_teplota)
+
+                # Zapsání aktuální teploty do Google Sheets tabulky
+                urllib.request.urlopen("https://script.google.com/macros/s/AKfycbwywgTKg7z5HZ20MCVtSzl-UiqNcUcTdc5_4C4Jzc3B03S89ykNE-S7Yt9UcOJT735I1g/exec?value=%s" % nova_teplota)
+
+                # Program bude odesílat upozornění pouze mezi 7:00 a 22:00
+                if((t.hour >= 7) and (t.hour <= 22)):
+
+                    # Pokud je celá hodina, spustit proces pro odeslání notifikace.
+                    if((t.minute == 0) and (t.second == 0)):
+
+                        # Zjistit teplotu minulou hodinu pro porovnání
+                        with open('teplota.txt', 'r') as f:
+                            stara_teplota = f.read()
+                            f.close()
+
+                        # Vypočítání rozílů teplot mezi hodinami
+                        rozdil_teplot = float(nova_teplota) - float(stara_teplota)
+
+                        # Vypočítání průměrné teloty za poslední hodinu a její zaokrouhlení
+                        prumerna_teplota = celkova_teplota/pocet
+                        prumerna_teplota = round(prumerna_teplota, 1)
+
+                        # Odeslání upozornění na telefon
+                        conn = http.client.HTTPSConnection("api.pushover.net:443")
+                        conn.request("POST", "/1/messages.json",
+                        urllib.parse.urlencode({
+                            "token": "a8fafvyjck1k2de5yz3hphcku3i499",
+                            "html": "1",
+                            "user": "u586cd4vg7qf4mtgert6buum515t8u",
+                            "title": "Teplota ve Dvoře: %s°C" % nova_teplota,
+                            "message": "Rozdíl teplot za uplynulou hodinu: <b>%s°C</b>\
+                            <br>Průměrná teplota: <b>%s°C</b>\
+                            <br>Minimální teplota: <b>%s°C</b>\
+                            <br>Maximální teplota: <b>%s°C</b>"\
+                            % (round(rozdil_teplot, 1), prumerna_teplota, round(minimalni_teplota, 1), round(maximalni_teplota, 1)),
+                        }), { "Content-type": "application/x-www-form-urlencoded" })
+                        conn.getresponse()
+                        print("Zpráva byla odeslána.")
+
+                        # Zapsat aktuální teplotu pro porovnání příští hodinu
+                        with open('teplota.txt', 'w') as f:
+                            stara_teplota = f.write(nova_teplota)
+                            f.close()
+
+                        # Nastavení proměnných na základní hodnoty
+                        celkova_teplota = 0.0
+                        maximalni_teplota = 0.0
+                        minimalni_teplota = 100.0
+                        
+    # Počkat 1 sekundu, aby se odeslal jen jeden záznam.
     time.sleep(1)
